1f5c3659bfff0f3ad3c03412f3e032552ff89cf4
alilsmth4ya <quyle83kd@gmail.com>
2023-09-25 18:31:48 +0700

    finished project

---
+++ app/src/main/java/com/example/quyle_login/Login.java
@@ -0,0 +1,32 @@
+package com.example.quyle_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignup();
+            }
+        });
+    }
+
+    public void openSignup(){
+        Intent intent = new Intent(this, Signup.class);
+        startActivity(intent);
+    }
+}
